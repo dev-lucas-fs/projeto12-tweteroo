a13353c0d540ab10da69692a368f6b979f00d478
Lucas F. Silva <lucasdotfs@gmail.com>
2022-11-04 15:36:02 -0300

    feat: adding get all tweets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 const PORT = 5000
 
 const app = express()
+app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
@@ -12,12 +13,12 @@ app.listen(PORT, () => {
 
 
 // SIGN-UP
-let user = []
+let users = []
 
 app.post("/sign-up", (request, response) => {
     const user = request.body
 
-    user.push(user)
+    users.push(user)
 
     response.send("OK")
 })
@@ -30,10 +31,21 @@ app.post("/tweets", (request, response) => {
     const tweet = request.body
 
     tweets.push(tweet)
-
+    
     response.send("OK")
 })
 
+
+app.get("/tweets", (request, response) => {
+
+    const lastTweets = tweets.map(tweet => {
+        const user = users.find(user => tweet.username == user.username)
+        if (user)
+            return {...tweet, avatar: user.avatar}
+    }).reverse().slice(0, 10)
+
+    response.send(lastTweets)
+})
 
 
 
